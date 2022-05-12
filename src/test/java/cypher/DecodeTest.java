ec29f896e6d9aa6b53e5df993598db5d5ec7186c
RikoStuff <chachaerickjo@gmail.com>
2022-05-12 13:11:57 +0300

    ft - add test file

---
+++ src/test/java/cypher/DecodeTest.java
@@ -0,0 +1,23 @@
+package cypher;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecodeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void input_acceptsOnlyCorrectInputType_true() {
+
+    }
+}
